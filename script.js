3335dd346621ec5550f4e69cba533fa8e66c786f
ipankajmehra <138882391+ipankajmehra@users.noreply.github.com>
2024-04-02 22:17:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function to handle updating a resume entry
 async function updateResume(data) {
-  const response = await fetch('YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL', {
+  const response = await fetch('https://script.google.com/a/macros/sageuniversity.in/s/AKfycbzqgIhgmVP6UvFkvhieOVYoieycYU-LHm-NScglAaQrBV19Eg1IWjQGdi3B6ck8N4kYUA/exec', {
     method: 'PUT',
     body: JSON.stringify(data),
     headers: {
@@ -18,7 +18,7 @@ async function updateResume(data) {
 
 // Function to handle deleting a resume entry
 async function deleteResume(data) {
-  const response = await fetch('YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL', {
+  const response = await fetch('https://script.google.com/a/macros/sageuniversity.in/s/AKfycbzqgIhgmVP6UvFkvhieOVYoieycYU-LHm-NScglAaQrBV19Eg1IWjQGdi3B6ck8N4kYUA/exec', {
     method: 'DELETE',
     body: JSON.stringify(data),
     headers: {
